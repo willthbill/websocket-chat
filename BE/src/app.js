3704940cb55a699f370865c244fc61c77debf8e6
williamBMDK <williambillemeyling@gmail.com>
2020-02-05 23:20:00 +0100

    multiple client support

--- BE/src/app.js
+++ BE/src/app.js
@@ -4,21 +4,23 @@ const wsserver = new (require("websocket")).server({
 	httpServer : server
 });
 let messages = [];
-const connections = [];
-const sendMessages = () => {
-	for(const connection of connections){
-		connection.send(JSON.stringify(messages));
+const connections = {};
+const sendMessageToAllClients = (message) => {
+	console.log(connections);
+	for(const ip in connections){
+		console.log(message);
+		connections[ip].send(JSON.stringify([message]));
 	}
 };
 wsserver.on("request", (req) => {
 	const connection = req.accept(null, req.origin);
-	connections.push(connection);
+	connections[connection.remoteAddress + " " + (new Date()).getTime()] = connection;
 	connection.send(JSON.stringify(messages));
 	connection.on("message", (mes) => {
 		if(mes.type == "utf8"){
 			console.log("message recieved");
 			messages.push(mes.utf8Data);
-			sendMessages();
+			sendMessageToAllClients(mes.utf8Data);
 		}
 	});
 });
